0927f08f9a8e3fb45d48724e5fb196cf3da6df53
Álvaro-code <alvaropereirasivaj@gmail.com>
2026-02-25 14:40:22 -0300

    Add files via upload

---
+++ tipagem.py
@@ -0,0 +1,11 @@
+nome = "Ada"
+print(type(nome))
+
+idade = 17
+print(type(idade))
+
+nota = 9.0
+print(type(nota))
+
+exemplo = True
+print(type(exemplo))
---
+++ variaveis.py
@@ -0,0 +1,7 @@
+nome = "Álvaro"
+
+var1 = 10
+Var1 = 12
+
+print(var1)
+print(Var1)
