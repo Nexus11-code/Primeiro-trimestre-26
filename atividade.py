f3c84893b8bfefa60fee005fc6070fcc31b07c34
Álvaro-code <alvaropereirasivaj@gmail.com>
2026-03-04 11:14:32 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,6 @@
+compras = ["arroz", "feijão", "leite", "pão", "ovos"]
+print(compras)
+
+novo_item = input("Digite um item para adicionar à lista de compras: ")
+compras.append(novo_item)
+print("Lista de compras atualizada: ", compras)
